f05b49980d8b4c4d90c24f2855b6402ebea8c2b4
njl <1012408214@qq.com>
2025-04-17 16:41:07 +0800

    Initial commit

---
+++ api/v1alpha1/kflow_types.go
@@ -0,0 +1,105 @@
+/*
+Copyright 2025.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// KflowSpec defines the desired state of Kflow.
+type KflowSpec struct {
+	// 任务定义
+	Tasks []TaskSpec `json:"tasks"`
+
+	// 分组策略（新增字段）
+	GroupPolicy GroupPolicy `json:"groupPolicy"`
+}
+
+type GroupPolicy struct {
+	Type     string `json:"type"`     // "Level" | "DataAffinity" | "Manual"
+	MaxTasks int    `json:"maxTasks"` // 每组最大任务数
+}
+
+type TaskSpec struct {
+	Name    string   `json:"name"`
+	Image   string   `json:"image"`
+	Command []string `json:"command"`
+
+	// +optional
+	Args []string `json:"args,omitempty"`
+
+	// +optional
+	Depends []string `json:"depends,omitempty"` //
+
+	// 数据路径（与 Volume 绑定）
+	InputPath  string `json:"inputPath,omitempty"`  // 例如 /data/input
+	OutputPath string `json:"outputPath,omitempty"` // 例如 /data/output
+}
+
+type KflowStatus struct {
+	Phase string `json:"phase"` // Processing | Completed | Failed
+
+	// 记录每个组的状态
+	Grouped    bool                  `json:"grouped,omitempty"`
+	Groups     []GroupStatus         `json:"groups,omitempty"`
+	Tasks      map[string]TaskStatus `json:"tasks,omitempty"`
+	GroupNodes []string              `json:"groupNodes,omitempty"`
+}
+
+type GroupStatus struct {
+	GroupID        int          `json:"groupId"`
+	Tasks          []TaskSpec   `json:"tasks"`  // 任务列表
+	Status         string       `json:"status"` // Pending | Running | Succeeded | Failed
+	StartTime      *metav1.Time `json:"startTime,omitempty"`
+	CompletionTime *metav1.Time `json:"completionTime,omitempty"`
+}
+
+type TaskStatus struct {
+	Task    TaskSpec `json:"task"`
+	Status  string   `json:"status"`
+	Node    string   `json:"node"`
+	Depends []string `json:"depends,omitempty"`
+	Pod     string   `json:"pod,omitempty"`
+}
+
+// +kubebuilder:object:root=true
+// +kubebuilder:subresource:status
+
+// Kflow is the Schema for the kflows API.
+type Kflow struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   KflowSpec   `json:"spec,omitempty"`
+	Status KflowStatus `json:"status,omitempty"`
+}
+
+// +kubebuilder:object:root=true
+
+// KflowList contains a list of Kflow.
+type KflowList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []Kflow `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&Kflow{}, &KflowList{})
+}
---
+++ internal/controller/kflow_controller.go
@@ -0,0 +1,355 @@
+/*
+Copyright 2025.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package controller
+
+import (
+	// 标准库
+	"context"
+	"fmt"
+	"hash/fnv"
+	"strings"
+
+	"apiserver/api/v1alpha1"
+	kflowiov1alpha1 "apiserver/api/v1alpha1"
+
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+)
+
+// KflowReconciler reconciles a Kflow object
+
+// KflowReconciler reconciles a Kflow object
+type KflowReconciler struct {
+	client.Client
+	Scheme *runtime.Scheme
+}
+
+// Reconcile 执行任务分配和调度
+func (r *KflowReconciler) Reconcile(ctx context.Context, req reconcile.Request) (reconcile.Result, error) {
+	log := log.FromContext(ctx)
+	log.Info("Reconciling Kflow111", "name", req.Name)
+
+	var kflow v1alpha1.Kflow
+	err_kflow := r.Get(ctx, req.NamespacedName, &kflow)
+	var pod corev1.Pod
+	err_pod := r.Get(ctx, req.NamespacedName, &pod)
+	ctrl.Log.Info("show reconcile Request",
+		"name", req.Name,
+		"namespace", req.Namespace,
+		"namespacename", req.NamespacedName)
+
+	if err_pod == nil {
+		log.Info("is pod")
+		if !strings.HasPrefix(pod.Name, "kflow-sample-") {
+			log.Info("pod name prefix uncorrect")
+			return reconcile.Result{}, client.IgnoreNotFound(err_kflow)
+		} else {
+			log.Info("is task pod",
+				"pod name", pod.Name)
+			parts := strings.Split(pod.Name, "-")
+			taskStautus := kflow.Status.Tasks[parts[2]]
+			taskStautus.Status = "compeleted"
+			kflow.Status.Tasks[parts[2]] = taskStautus
+
+			for _, Group := range kflow.Status.Groups {
+				err := r.scheduleTasks(&kflow, Group.Tasks)
+				if err != nil {
+					log.Error(err, "Failed to schedule tasks")
+					return reconcile.Result{}, err
+				}
+			}
+
+		}
+
+	}
+
+	if err_kflow == nil {
+		if kflow.Status.Grouped == false {
+			// Step 1: 分配分组node
+			ctrl.Log.Info("start group nodes")
+			nodeList := &corev1.NodeList{}
+			if err := r.List(context.Background(), nodeList); err != nil {
+				return reconcile.Result{}, err
+			}
+			groupCount := len(nodeList.Items)
+			ctrl.Log.Info("show node count", "node count", groupCount)
+
+			kflow.Status.GroupNodes = make([]string, groupCount)
+			kflow.Status.Tasks = make(map[string]kflowiov1alpha1.TaskStatus)
+
+			for i := 0; i < groupCount; i++ {
+				selectedNode := nodeList.Items[i].Name
+				kflow.Status.GroupNodes[i] = selectedNode
+			}
+
+			//Step3：任务分组
+			err := r.groupTasks(&kflow)
+			if err != nil {
+				log.Error(err, "Failed to group tasks")
+				return reconcile.Result{}, err
+			}
+		}
+
+		for _, Group := range kflow.Status.Groups {
+			err := r.scheduleTasks(&kflow, Group.Tasks)
+			if err != nil {
+				log.Error(err, "Failed to schedule tasks")
+				return reconcile.Result{}, err
+			}
+		}
+
+	} else {
+		log.Info("not kflow or pod")
+		return reconcile.Result{}, client.IgnoreNotFound(err_kflow)
+	}
+
+	if err := r.Status().Update(ctx, &kflow); err != nil {
+		log.Error(err, "Failed to update Kflow status")
+		return reconcile.Result{}, err
+	}
+
+	return reconcile.Result{}, nil
+}
+
+// groupTasks 将任务分组
+func (r *KflowReconciler) groupTasks(kflow *v1alpha1.Kflow) error {
+	ctrl.Log.Info("Start group Tasks")
+	groupedTasks := make(map[int][]kflowiov1alpha1.TaskSpec)
+	kflow.Status.Groups = r.SetGroupStatus(groupedTasks, "pending")
+	groupPolicy := kflow.Spec.GroupPolicy
+
+	// 按照分组策略进行分组
+	switch groupPolicy.Type {
+	case "Level":
+		// Level 类型：轮询分组
+		for i, task := range kflow.Spec.Tasks {
+			groupID := i / groupPolicy.MaxTasks
+			groupedTasks[groupID] = append(groupedTasks[groupID], task)
+			kflow.Status.Tasks[task.Name] = r.CreateTaskStatus(groupID, task, kflow)
+		}
+	case "DataAffinity":
+		// DataAffinity 类型：按数据路径分组（假设这里是基于 InputPath 或 OutputPath）
+		for _, task := range kflow.Spec.Tasks {
+			groupID := hashDataPath(task.InputPath) % groupPolicy.MaxTasks
+			groupedTasks[groupID] = append(groupedTasks[groupID], task)
+			kflow.Status.Tasks[task.Name] = r.CreateTaskStatus(groupID, task, kflow)
+		}
+	case "Manual":
+		// Manual 类型：由用户自定义，可以用元数据来分配任务到组
+		// 示例：按任务名称进行分组（假设用户已经在任务描述里提供了分组策略）
+		for i, task := range kflow.Spec.Tasks {
+			groupID := i / groupPolicy.MaxTasks
+			groupedTasks[groupID] = append(groupedTasks[groupID], task)
+			kflow.Status.Tasks[task.Name] = r.CreateTaskStatus(groupID, task, kflow)
+		}
+	default:
+		return nil
+	}
+
+	kflow.Status.Groups = r.SetGroupStatus(groupedTasks, "runnning")
+	kflow.Status.Grouped = true
+	return nil
+}
+
+func (r *KflowReconciler) CreateTaskStatus(groupID int, task kflowiov1alpha1.TaskSpec, kflow *kflowiov1alpha1.Kflow) kflowiov1alpha1.TaskStatus {
+	taskstatus := kflowiov1alpha1.TaskStatus{
+		Task:    task,
+		Status:  "create",
+		Node:    kflow.Status.GroupNodes[groupID],
+		Depends: task.Depends,
+	}
+	return taskstatus
+}
+
+func hashDataPath(s string) int {
+	h := fnv.New32a()
+	h.Write([]byte(s))
+	return int(h.Sum32())
+}
+
+// scheduleTasks 为每个任务组选择一个节点，并为任务创建 Pod
+func (r *KflowReconciler) scheduleTasks(kflow *v1alpha1.Kflow, Tasks []kflowiov1alpha1.TaskSpec) error {
+	ctrl.Log.Info("Start schedule Tasks")
+
+	// 为每个任务创建 Pod
+	for _, TaskSpec := range Tasks {
+		if r.CheckDependsStatus(*kflow, TaskSpec) {
+			ctrl.Log.Info("start build pod", "task name", TaskSpec.Name)
+			pod := &corev1.Pod{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      fmt.Sprintf("%s-%s", kflow.Name, TaskSpec.Name),
+					Namespace: "kflow-worker",
+				},
+				Spec: corev1.PodSpec{
+					RestartPolicy: corev1.RestartPolicyNever,
+					NodeSelector: map[string]string{
+						"kubernetes.io/hostname": kflow.Status.Tasks[TaskSpec.Name].Node, // 替换为目标节点的名称
+					},
+					Containers: []corev1.Container{
+						{
+							Name:    TaskSpec.Name,
+							Image:   TaskSpec.Image, // 需要替换为实际的任务镜像
+							Command: TaskSpec.Command,
+						},
+					},
+					//Affinity: &corev1.Affinity{
+					//	NodeAffinity: &corev1.NodeAffinity{
+					//		RequiredDuringSchedulingIgnoredDuringExecution: &corev1.NodeSelector{
+					//			NodeSelectorTerms: []corev1.NodeSelectorTerm{
+					//				{
+					//					MatchExpressions: []corev1.NodeSelectorRequirement{
+					//						{
+					//							Key:      "kubernetes.io/hostname",
+					//							Operator: corev1.NodeSelectorOpIn,
+					//							Values:   []string{nodeName},
+					//						},
+					//					},
+					//				},
+					//			},
+					//		},
+					//	},
+					//},
+				},
+			}
+
+			ctrl.Log.Info("start create contrainer")
+			if err := r.Create(context.Background(), pod); err != nil {
+				return err
+			}
+			taskStatus := kflow.Status.Tasks[TaskSpec.Name]
+			taskStatus.Pod = pod.Name
+			taskStatus.Status = "Running"
+			kflow.Status.Tasks[TaskSpec.Name] = taskStatus
+		}
+	}
+	return nil
+}
+
+func (r *KflowReconciler) CheckDependsStatus(kflow kflowiov1alpha1.Kflow, TaskSpec kflowiov1alpha1.TaskSpec) bool {
+	log.Log.Info("Check Depends Status",
+		"current task", TaskSpec.Name)
+	depends := kflow.Status.Tasks[TaskSpec.Name].Depends
+	for _, taskName := range depends {
+		log.Log.Info("check Depend",
+			"depend name", taskName,
+			"status", kflow.Status.Tasks[taskName].Status)
+		if kflow.Status.Tasks[taskName].Status != "compeleted" {
+			return false
+		}
+	}
+	return true
+}
+
+//func (r *KflowReconciler) SetTaskStatus(kflow kflowiov1alpha1.Kflow, pod corev1.Pod) error {
+//
+//}
+
+// createGroupStatus 生成每个组的状态
+func (r *KflowReconciler) SetGroupStatus(groupedTasks map[int][]kflowiov1alpha1.TaskSpec, status string) []v1alpha1.GroupStatus {
+	var groups []v1alpha1.GroupStatus
+	for groupID, tasks := range groupedTasks {
+		group := v1alpha1.GroupStatus{
+			GroupID: groupID,
+			Tasks:   tasks,
+			Status:  status,
+		}
+		groups = append(groups, group)
+	}
+	return groups
+}
+
+// SetupWithManager sets up the controller with the Manager.
+func (r *KflowReconciler) SetupWithManager(mgr ctrl.Manager) error {
+	// 设置 Kflow 资源的 Watch
+	err := ctrl.NewControllerManagedBy(mgr).
+		For(&kflowiov1alpha1.Kflow{}).
+		Complete(r)
+	if err != nil {
+		return err
+	}
+
+	// 设置 Pod 资源的 Watch，并使用事件过滤器
+	/*err = ctrl.NewControllerManagedBy(mgr).
+	For(&corev1.Pod{}).
+	WithEventFilter(predicate.Funcs{
+		UpdateFunc: func(e event.UpdateEvent) bool {
+			pod := e.ObjectNew.(*corev1.Pod)
+
+			// 使用正则表达式匹配 Pod 名称是否以 "kflow-sample-" 为前缀
+			matched, _ := regexp.MatchString("^kflow-sample-", pod.GetName())
+			if !matched {
+				return false // 只处理名称以 "kflow-sample-" 开头的 Pod
+			}
+
+			// 其他过滤条件：Pod 的状态必须是 Succeeded 或 Failed，且命名空间是 "kflow-worker"
+			return (pod.Status.Phase == corev1.PodSucceeded || pod.Status.Phase == corev1.PodFailed) &&
+				pod.GetNamespace() == "kflow-worker"
+		},
+	}).
+	Complete(r)*/
+	return err
+}
+
+/*func (r *KflowReconciler) SetupWithManager(mgr ctrl.Manager) error {
+	return ctrl.NewControllerManagedBy(mgr).
+		For(&kflowiov1alpha1.Kflow{}).
+		// 监听 Pod 资源
+		Watches(
+			builder.Kind(mgr.GetScheme(), &corev1.Pod{}),
+			handler.EnqueueRequestsFromMapFunc(handler.MapFunc(func(obj client.Object) []reconcile.Request {
+				pod, ok := obj.(*corev1.Pod)
+				if !ok {
+					return nil
+				}
+
+				if !strings.HasPrefix(pod.GetName(), "kflow-sample-") {
+					return nil
+				}
+
+				if pod.GetNamespace() != "kflow-worker" ||
+					(pod.Status.Phase != corev1.PodSucceeded && pod.Status.Phase != corev1.PodFailed) {
+					return nil
+				}
+
+				kflowName := pod.Labels["kflow-owner"]
+				if kflowName == "" {
+					return nil
+				}
+
+				return []reconcile.Request{
+					{
+						NamespacedName: client.ObjectKey{
+							Name:      kflowName,
+							Namespace: pod.Namespace,
+						},
+					},
+				}
+			})),
+			builder.WithPredicates(predicate.Funcs{
+				UpdateFunc: func(e event.UpdateEvent) bool {
+					newPod := e.ObjectNew.(*corev1.Pod)
+					return newPod.Status.Phase == corev1.PodSucceeded || newPod.Status.Phase == corev1.PodFailed
+				},
+			}),
+		).
+		Complete(r)
+}*/
