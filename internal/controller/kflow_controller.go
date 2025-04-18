89446f60b628eaec4355b889382500cff2daa766
njl <1012408214@qq.com>
2025-04-18 16:23:00 +0800

    modified:   config/rbac/role.yaml 	modified:   config/samples/kflow.io_v1alpha1_kflow.yaml 	modified:   internal/controller/kflow_controller.go 	modified:   make-build.sh

--- internal/controller/kflow_controller.go
+++ internal/controller/kflow_controller.go
@@ -21,6 +21,7 @@ import (
 	"context"
 	"fmt"
 	"hash/fnv"
+	"regexp"
 	"strings"
 
 	"apiserver/api/v1alpha1"
@@ -29,9 +30,12 @@ import (
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/event"
 	"sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/predicate"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 )
 
@@ -46,29 +50,58 @@ type KflowReconciler struct {
 // Reconcile 执行任务分配和调度
 func (r *KflowReconciler) Reconcile(ctx context.Context, req reconcile.Request) (reconcile.Result, error) {
 	log := log.FromContext(ctx)
-	log.Info("Reconciling Kflow111", "name", req.Name)
+	//log.Info("Reconciling Kflow111", "name", req.Name)
 
 	var kflow v1alpha1.Kflow
 	err_kflow := r.Get(ctx, req.NamespacedName, &kflow)
 	var pod corev1.Pod
 	err_pod := r.Get(ctx, req.NamespacedName, &pod)
-	ctrl.Log.Info("show reconcile Request",
-		"name", req.Name,
-		"namespace", req.Namespace,
-		"namespacename", req.NamespacedName)
+	/*ctrl.Log.Info("show reconcile Request",
+	"name", req.Name,
+	"namespace", req.Namespace,
+	"namespacename", req.NamespacedName)*/
 
-	if err_pod == nil {
-		log.Info("is pod")
+	if err_pod == nil && err_kflow != nil {
+		//log.Info("is pod")
+
 		if !strings.HasPrefix(pod.Name, "kflow-sample-") {
-			log.Info("pod name prefix uncorrect")
+			//log.Info("pod name prefix uncorrect")
 			return reconcile.Result{}, client.IgnoreNotFound(err_kflow)
 		} else {
 			log.Info("is task pod",
 				"pod name", pod.Name)
 			parts := strings.Split(pod.Name, "-")
+			//time.Sleep(3 * time.Second)
+
+			var kflow kflowiov1alpha1.Kflow
+			kflowKey := types.NamespacedName{
+				Name:      "kflow-sample", // 假设 Kflow 资源的名称为 "kflow-sample"
+				Namespace: "default",      // 假设 Kflow 资源的命名空间为 "default"
+			}
+			err := r.Get(ctx, kflowKey, &kflow)
+			if err != nil {
+				log.Error(err_kflow, "failed to get Kflow resource")
+				return reconcile.Result{}, err_kflow
+			}
+
+			if kflow.Status.Tasks == nil {
+				log.Info("Status update not ready")
+				return reconcile.Result{}, err
+			}
+			// 成功获取 Kflow 资源后，你可以在这里继续处理
+			//log.Info("checking task status", "task name", parts[2])
+			//log.Info("show task status", "status", kflow.Status.Tasks)
+			//log.Info("show status", "status", kflow.Status)
+			log.Info("show task status",
+				"task name", parts[2],
+				"task status", kflow.Status.Tasks[parts[2]])
 			taskStautus := kflow.Status.Tasks[parts[2]]
-			taskStautus.Status = "compeleted"
+			taskStautus.Status = "completed"
 			kflow.Status.Tasks[parts[2]] = taskStautus
+			if err := r.Status().Update(ctx, &kflow); err != nil {
+				log.Error(err, "Failed to update Kflow status")
+				return reconcile.Result{}, err
+			}
 
 			for _, Group := range kflow.Status.Groups {
 				err := r.scheduleTasks(&kflow, Group.Tasks)
@@ -82,7 +115,8 @@ func (r *KflowReconciler) Reconcile(ctx context.Context, req reconcile.Request) 
 
 	}
 
-	if err_kflow == nil {
+	if err_kflow == nil && err_pod != nil {
+		log.Info("is kflow")
 		if kflow.Status.Grouped == false {
 			// Step 1: 分配分组node
 			ctrl.Log.Info("start group nodes")
@@ -107,24 +141,24 @@ func (r *KflowReconciler) Reconcile(ctx context.Context, req reconcile.Request) 
 				log.Error(err, "Failed to group tasks")
 				return reconcile.Result{}, err
 			}
-		}
 
-		for _, Group := range kflow.Status.Groups {
-			err := r.scheduleTasks(&kflow, Group.Tasks)
-			if err != nil {
-				log.Error(err, "Failed to schedule tasks")
-				return reconcile.Result{}, err
+			for _, Group := range kflow.Status.Groups {
+				err := r.scheduleTasks(&kflow, Group.Tasks)
+				if err != nil {
+					log.Error(err, "Failed to schedule tasks")
+					return reconcile.Result{}, err
+				}
 			}
 		}
-
-	} else {
-		log.Info("not kflow or pod")
-		return reconcile.Result{}, client.IgnoreNotFound(err_kflow)
+		if err := r.Status().Update(ctx, &kflow); err != nil {
+			log.Error(err, "Failed to update Kflow status")
+			return reconcile.Result{}, err
+		}
 	}
 
-	if err := r.Status().Update(ctx, &kflow); err != nil {
-		log.Error(err, "Failed to update Kflow status")
-		return reconcile.Result{}, err
+	if err_kflow != nil && err_pod != nil {
+		log.Info("not kflow or pod", "request name", req.Name)
+		return reconcile.Result{}, client.IgnoreNotFound(err_kflow)
 	}
 
 	return reconcile.Result{}, nil
@@ -145,6 +179,9 @@ func (r *KflowReconciler) groupTasks(kflow *v1alpha1.Kflow) error {
 			groupID := i / groupPolicy.MaxTasks
 			groupedTasks[groupID] = append(groupedTasks[groupID], task)
 			kflow.Status.Tasks[task.Name] = r.CreateTaskStatus(groupID, task, kflow)
+			ctrl.Log.Info("show created status",
+				"task ", task.Name,
+				"status", kflow.Status.Tasks[task.Name])
 		}
 	case "DataAffinity":
 		// DataAffinity 类型：按数据路径分组（假设这里是基于 InputPath 或 OutputPath）
@@ -192,6 +229,11 @@ func (r *KflowReconciler) scheduleTasks(kflow *v1alpha1.Kflow, Tasks []kflowiov1
 
 	// 为每个任务创建 Pod
 	for _, TaskSpec := range Tasks {
+
+		if kflow.Status.Tasks[TaskSpec.Name].Status == "completed" {
+			log.Log.Info("task completed", "task name", TaskSpec.Name, "status", kflow.Status.Tasks[TaskSpec.Name])
+			continue
+		}
 		if r.CheckDependsStatus(*kflow, TaskSpec) {
 			ctrl.Log.Info("start build pod", "task name", TaskSpec.Name)
 			pod := &corev1.Pod{
@@ -240,6 +282,7 @@ func (r *KflowReconciler) scheduleTasks(kflow *v1alpha1.Kflow, Tasks []kflowiov1
 			taskStatus.Status = "Running"
 			kflow.Status.Tasks[TaskSpec.Name] = taskStatus
 		}
+
 	}
 	return nil
 }
@@ -252,7 +295,7 @@ func (r *KflowReconciler) CheckDependsStatus(kflow kflowiov1alpha1.Kflow, TaskSp
 		log.Log.Info("check Depend",
 			"depend name", taskName,
 			"status", kflow.Status.Tasks[taskName].Status)
-		if kflow.Status.Tasks[taskName].Status != "compeleted" {
+		if kflow.Status.Tasks[taskName].Status != "completed" {
 			return false
 		}
 	}
@@ -288,24 +331,24 @@ func (r *KflowReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	}
 
 	// 设置 Pod 资源的 Watch，并使用事件过滤器
-	/*err = ctrl.NewControllerManagedBy(mgr).
-	For(&corev1.Pod{}).
-	WithEventFilter(predicate.Funcs{
-		UpdateFunc: func(e event.UpdateEvent) bool {
-			pod := e.ObjectNew.(*corev1.Pod)
+	err = ctrl.NewControllerManagedBy(mgr).
+		For(&corev1.Pod{}).
+		WithEventFilter(predicate.Funcs{
+			UpdateFunc: func(e event.UpdateEvent) bool {
+				pod := e.ObjectNew.(*corev1.Pod)
 
-			// 使用正则表达式匹配 Pod 名称是否以 "kflow-sample-" 为前缀
-			matched, _ := regexp.MatchString("^kflow-sample-", pod.GetName())
-			if !matched {
-				return false // 只处理名称以 "kflow-sample-" 开头的 Pod
-			}
+				// 使用正则表达式匹配 Pod 名称是否以 "kflow-sample-" 为前缀
+				matched, _ := regexp.MatchString("^kflow-sample-", pod.GetName())
+				if !matched {
+					return false // 只处理名称以 "kflow-sample-" 开头的 Pod
+				}
 
-			// 其他过滤条件：Pod 的状态必须是 Succeeded 或 Failed，且命名空间是 "kflow-worker"
-			return (pod.Status.Phase == corev1.PodSucceeded || pod.Status.Phase == corev1.PodFailed) &&
-				pod.GetNamespace() == "kflow-worker"
-		},
-	}).
-	Complete(r)*/
+				// 其他过滤条件：Pod 的状态必须是 Succeeded 或 Failed，且命名空间是 "kflow-worker"
+				return (pod.Status.Phase == corev1.PodSucceeded || pod.Status.Phase == corev1.PodFailed) &&
+					pod.GetNamespace() == "kflow-worker"
+			},
+		}).
+		Complete(r)
 	return err
 }
 
--- make-build.sh
+++ make-build.sh
@@ -1,10 +1,11 @@
+kubectl delete deployment kflow-controller-manager -n kflow-system
 make docker-build
 docker tag $(docker image ls --format "{{.Repository}} {{.Tag}} {{.ID}} {{.CreatedSince}}" | grep controller | grep latest | head -n 1 | awk '{print $3}'
 ) crpi-2rclh8j1lqwo45m4.cn-qingdao.personal.cr.aliyuncs.com/mnikube/controller
 docker push crpi-2rclh8j1lqwo45m4.cn-qingdao.personal.cr.aliyuncs.com/mnikube/controller
-make deploy
 kubectl delete pod --all -n kflow-worker
 kubectl delete crd --all
 kubectl apply -f ./config/crd/bases/kflow.io.kflow_kflows.yaml 
-kubectl delete pod $(kubectl get pods -n kflow-system | grep kflow-controller | awk '{print $1}') -n kflow-system
 kubectl apply -f /home/njl/kflow/config/samples/kflow.io_v1alpha1_kflow.yaml
+make deploy
+#kubectl delete pod $(kubectl get pods -n kflow-system | grep kflow-controller | awk '{print $1}') -n kflow-system
