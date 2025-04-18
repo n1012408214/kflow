8318520c6bfaca7c35126a8692816b0c3a4f6c45
njl <1012408214@qq.com>
2025-04-18 16:20:52 +0800

    complete chain workflow

--- make-build.sh
+++ make-build.sh
@@ -4,7 +4,6 @@ docker tag $(docker image ls --format "{{.Repository}} {{.Tag}} {{.ID}} {{.Creat
 docker push crpi-2rclh8j1lqwo45m4.cn-qingdao.personal.cr.aliyuncs.com/mnikube/controller
 make deploy
 kubectl delete pod --all -n kflow-worker
-kubectl delete pod --all
 kubectl delete crd --all
 kubectl apply -f ./config/crd/bases/kflow.io.kflow_kflows.yaml 
 kubectl delete pod $(kubectl get pods -n kflow-system | grep kflow-controller | awk '{print $1}') -n kflow-system
